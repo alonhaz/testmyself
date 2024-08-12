b861dd22f4059b5d2168b750c9da12c8c4661aae
Alon Hazan <alonhaz02@gmail.com>
2024-08-12 16:01:00 +0300

    test34

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './pages/Home';
 import ProtectedRoute from './components/ProtectedRoute';
