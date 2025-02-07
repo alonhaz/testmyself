24aff36bfe654e4386724161acd662c1b22b2bee
Alon Hazan <alonhaz02@gmail.com>
2025-02-07 13:40:27 +0000

    NEW DESIGN

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allows you to specify the IP address
+    //port: 5173, // You can change the port if needed
+  }
 });
